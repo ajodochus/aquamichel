f3d353f6d8372cf43fef1f7e937bcf541e20b72b
ajodochus <ajodochus@gmail.com>
2025-05-02 14:01:35 +0200

    Server vars

--- src/server.cpp
+++ src/server.cpp
@@ -4,6 +4,7 @@
 #include <ESPAsyncWebServer.h>
 #include "server.h"
 #include "component_display.h"
+#include "service.h"
 
 // Declare the server object
 AsyncWebServer server(80);
@@ -14,13 +15,33 @@ const char index_html[] PROGMEM = R"rawliteral(
 <html>
 <head><title>ESP32 Async WebServer</title></head>
 <body>
-  <h1>Hello from ESP32 Async Server!</h1>
+  <h1>Aquamichel</h1>
+  <p>var1: %VALUE1%</p>
+  <p>var2: %VALUE2%</p>
+  
+    <p>Timer: <span id="current_time">...</span></p>
+
+    <script>
+      setInterval(() => {
+        fetch("/current_time")
+          .then(response => response.text())
+          .then(data => {
+            document.getElementById("current_time").textContent = data;
+          });
+      }, 1000);
+    </script>
 </body>
 </html>
 )rawliteral";
 
 // Optional template processor
 String processor(const String& var) {
+  if(var == "VALUE1"){
+    return String("value1");
+  }
+  if(var == "VALUE2"){
+    return String("value2");
+  }
   return String();
 }
 
@@ -43,6 +64,9 @@ void startWiFiAndServer(const char* ssid, const char* password) {
   // Define server routes
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send_P(200, "text/html", index_html, processor);
+  });
+   server.on("/current_time", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plain", String(timer_current_time));
   });
 
   server.begin();
